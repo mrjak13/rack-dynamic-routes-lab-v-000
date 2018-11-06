cb8ac52b63569bb028ce5e695020ab95a8085026
Justin Kenney <justinkenney13@gmail.com>
2018-11-06 19:39:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,15 @@
 class Aplication
 
-  def call(env)      
+  def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
-    @@items.detect {|item| item.name == item_name}.price
-  
+    resp.write "#{@@items.detect{|item| item.name == item_name}.price}"
+  else
+
+
   resp.finish
   end
 end
